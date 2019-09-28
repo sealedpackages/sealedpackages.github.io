6c61d9687338e63ce87f7f6651d1b55fe9b11c9a
Matt <12mdeyo@gmail.com>
2019-09-28 15:44:58 -0700

    pricing section simpler

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -62,14 +62,14 @@
 		 
 		 $('.mu-menu-full-overlay').removeClass('mu-menu-full-overlay-show');
 
-         //calculate destination place
+        //  calculate destination place
          var dest=0;
          if($(this.hash).offset().top > $(document).height()-$(window).height()){
               dest=$(document).height()-$(window).height();
          }else{
               dest=$(this.hash).offset().top;
          }
-         //go to destination
+        //  go to destination
          $('html,body').animate({scrollTop:dest}, 500,'linear');
      });
 	    
